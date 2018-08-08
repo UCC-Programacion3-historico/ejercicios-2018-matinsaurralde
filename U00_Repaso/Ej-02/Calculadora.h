c3664603b4c53de69055f47a0760b58b74dc38d9
matinsaurralde <matinsaurralde4@gmail.com>
2018-08-08 10:38:46 +0000

    ej 2 y 3(calculadora con excepciones)

--- U00_Repaso/Ej-02/Calculadora.h
+++ U00_Repaso/Ej-02/Calculadora.h
@@ -1,10 +1,43 @@
 #ifndef REPASO_CALCULADORA_H
 #define REPASO_CALCULADORA_H
 
+#include <iostream>
 
-class Calculadora {
+using namespace std;
 
-};
+
+template <class T, class P>
+        class calculadora {
+        public:
+        T sumar(T a, T b){
+            return a+b;
+        }
+
+            T restar(T a, T b){
+                return a-b;
+            }
+
+            T multiplicar(T a, T b){
+                return a*b;
+            }
+
+            T dividir(T a, T b){
+            T tmp;
+
+            if (b==0)
+                throw 404;
+
+                return a/b;
+
+            }
+
+            bool igual (T a, T b){
+            return a == b;
+        }
+
+
+
+        };
 
 
 #endif //REPASO_CALCULADORA_H
--- U00_Repaso/Ej-02/main.cpp
+++ U00_Repaso/Ej-02/main.cpp
@@ -4,6 +4,28 @@
 using namespace std;
 
 int main() {
+
+
+    calculadora <int, long> micalc;
+    calculadora <double,long> micalcDouble;
     cout << "Ejercicio 00/02\n" << endl;
+    cout << micalc.sumar(2,2)<< endl;
+    cout << micalc.restar(2,3)<< endl;
+    cout << micalc.multiplicar(3,2)<< endl;
+    cout << micalc.dividir(6,2)<< endl;
+
+    try {
+        cout << micalc.dividir(4,0) << endl;
+    }catch (int e) {
+        cout<< "error: "<< e << endl;
+    }
+
+
+
+    cout << micalcDouble.sumar(2,2)<< endl;
+    cout << micalcDouble.restar(2,3)<< endl;
+    cout << micalcDouble.multiplicar(3,2)<< endl;
+    cout << micalcDouble.dividir(6,5)<< endl;
+
     return 0;
 }
